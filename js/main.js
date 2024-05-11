07c16d352c7f8d626b6ed05e30cfc254c5ed9ff4
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-05-10 20:48:51 -0400

    header mobile friendly

--- js/main.js
+++ js/main.js
@@ -4,5 +4,13 @@ $(function () {
       '<button class="slick-arrow slick-prev"><img src="../images/prev.svg" alt="prev arrow"></button>',
     nextArrow:
       '<button class="slick-arrow slick-next"><img src="../images/next.svg" alt="next arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
